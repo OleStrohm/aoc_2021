1fa3a917d87aa74e79da6443ae8699872f72fa29
OleStrohm <strohm99@gmail.com>
2021-12-08 11:53:29 +0000

    Day 8

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,87 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-mod previous_days;
-
-use std::ops::Sub;
+use std::collections::{HashMap, HashSet};
+use std::io::BufRead;
 
 use itertools::Itertools;
 
-fn sum_1_to_n(n: i64) -> i64 {
-    n * (n + 1) / 2
-}
-
-fn day7() {
-    let s = include_str!("day7.txt");
-    let positions = s
-        .trim_end()
-        .split(',')
-        .map(|n| dbg!(n).parse::<i64>().unwrap())
-        .sorted()
-        .collect_vec();
-    let (&min, &max) = positions.iter().minmax().into_option().unwrap();
-    let least_fuel = (min..=max)
-        .map(|p| positions.iter().map(|cp| cp.sub(p).abs()).sum::<i64>())
-        .min()
-        .unwrap();
-    println!("part 1: {:?}", least_fuel,);
-
-    let least_fuel = (min..=max)
-        .map(|p| {
-            positions
-                .iter()
-                .map(|cp| sum_1_to_n(cp.sub(p).abs()))
-                .sum::<i64>()
-        })
-        .min()
-        .unwrap();
-    println!("part 2: {:?}", least_fuel,);
-}
+mod previous_days;
 
 fn main() {
-    day7();
+    let s = include_str!("day8.txt");
+    let (input, output): (Vec<_>, Vec<_>) = s
+        .lines()
+        .map(|l| l.split(" | ").collect_tuple::<(&str, &str)>().unwrap())
+        .unzip();
+
+    let num_1478 = output
+        .iter()
+        .flat_map(|l| l.split(" "))
+        .filter(|n| matches!(n.len(), 2 | 3 | 4 | 7))
+        .count();
+    println!("part 1: {}", num_1478);
+
+    let sum = input
+        .into_iter()
+        .zip(output.into_iter())
+        .fold(0, |s, (i, o)| {
+            let input = i.split(' ').collect_vec();
+
+            fn fits_in(inner: &str, outer: &str) -> bool {
+                inner.chars().all(|c| outer.contains(c))
+            }
+            fn equals(a: &str, b: &str) -> bool {
+                fits_in(a, b) && fits_in(b, a)
+            }
+            fn extract<'a>(collection: &'_ [&'a str], pred: impl FnMut(&&&str) -> bool) -> &'a str {
+                collection.into_iter().filter(pred).next().unwrap()
+            }
+
+            let one = extract(&input, |n| n.len() == 2);
+            let seven = extract(&input, |n| n.len() == 3);
+            let four = extract(&input, |n| n.len() == 4);
+            let eight = extract(&input, |n| n.len() == 7);
+
+            let one_of_235 = input.iter().filter(|n| n.len() == 5).copied().collect_vec();
+            let one_of_069 = input.iter().filter(|n| n.len() == 6).copied().collect_vec();
+
+            let (zero, six, nine) = one_of_069
+                .into_iter()
+                .sorted_by_key(|n| one_of_235.iter().filter(|i| fits_in(i, n)).count())
+                .collect_tuple()
+                .unwrap();
+
+            let five = extract(&one_of_235, |n| fits_in(n, six));
+            let three = extract(&one_of_235, |n| fits_in(one, n));
+
+            let mapping = vec![
+                (zero, 0),
+                (one, 1),
+                (three, 3),
+                (four, 4),
+                (five, 5),
+                (six, 6),
+                (seven, 7),
+                (eight, 8),
+                (nine, 9),
+            ];
+
+            let n = o
+                .split(' ')
+                .rev()
+                .enumerate()
+                .map(|(i, n)| {
+                    10_u64.pow(i as _)
+                        * mapping
+                            .iter()
+                            .find(|(c, _)| equals(n, c))
+                            .map_or(2, |(_, v)| *v)
+                })
+                .sum::<u64>();
+
+            s + n
+        });
+
+    println!("part 2: {}", sum);
 }
--- src/previous_days.rs
+++ src/previous_days.rs
@@ -1,6 +1,38 @@
 use itertools::Itertools;
 use rayon::prelude::*;
 use std::collections::{HashMap, HashSet};
+use std::ops::Sub;
+
+fn sum_1_to_n(n: i64) -> i64 {
+    n * (n + 1) / 2
+}
+
+fn day7() {
+    let s = include_str!("day7.txt");
+    let positions = s
+        .trim_end()
+        .split(',')
+        .map(|n| dbg!(n).parse::<i64>().unwrap())
+        .sorted()
+        .collect_vec();
+    let (&min, &max) = positions.iter().minmax().into_option().unwrap();
+    let least_fuel = (min..=max)
+        .map(|p| positions.iter().map(|cp| cp.sub(p).abs()).sum::<i64>())
+        .min()
+        .unwrap();
+    println!("part 1: {:?}", least_fuel,);
+
+    let least_fuel = (min..=max)
+        .map(|p| {
+            positions
+                .iter()
+                .map(|cp| sum_1_to_n(cp.sub(p).abs()))
+                .sum::<i64>()
+        })
+        .min()
+        .unwrap();
+    println!("part 2: {:?}", least_fuel,);
+}
 
 fn step(state: &[(u64, u64)]) -> Vec<(u64, u64)> {
     let new = state.get(0).map_or(0, |&(_, k)| k) + state.get(2).map_or(0, |&(a, _)| a);
