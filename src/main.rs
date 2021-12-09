8ca4898e8c8bf3745640a17d9d2fd109d69768f0
OleStrohm <strohm99@gmail.com>
2021-12-09 12:18:00 +0000

    Day 9 is has mutable state :(

--- src/main.rs
+++ src/main.rs
@@ -2,86 +2,126 @@
 #![allow(unused)]
 
 use std::collections::{HashMap, HashSet};
-use std::io::BufRead;
 
 use itertools::Itertools;
 
 mod previous_days;
 
 fn main() {
-    let s = include_str!("day8.txt");
-    let (input, output): (Vec<_>, Vec<_>) = s
+    let s = include_str!("day9.txt");
+    let grid = s
         .lines()
-        .map(|l| l.split(" | ").collect_tuple::<(&str, &str)>().unwrap())
-        .unzip();
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .collect_vec();
 
-    let num_1478 = output
+    let low_points = grid
         .iter()
-        .flat_map(|l| l.split(" "))
-        .filter(|n| matches!(n.len(), 2 | 3 | 4 | 7))
-        .count();
-    println!("part 1: {}", num_1478);
+        .enumerate()
+        .flat_map(|(y, r)| {
+            let grid = &grid;
+            r.iter().enumerate().filter_map(move |(x, n)| {
+                let up = y
+                    .checked_sub(1)
+                    .and_then(|y| grid.get(y))
+                    .and_then(|r| r.get(x));
+                let down = grid.get(y + 1).and_then(|r| r.get(x));
+                let left = grid
+                    .get(y)
+                    .and_then(|r| x.checked_sub(1).and_then(|x| r.get(x)));
+                let right = grid.get(y).and_then(|r| r.get(x + 1));
 
-    let sum = input
-        .into_iter()
-        .zip(output.into_iter())
-        .fold(0, |s, (i, o)| {
-            let input = i.split(' ').collect_vec();
+                up.into_iter()
+                    .chain(down)
+                    .chain(left)
+                    .chain(right)
+                    .min()
+                    .and_then(|min| (n < min).then(|| (x, y, n)))
+            })
+        })
+        .collect_vec();
+    let risk_level = low_points.iter().map(|(_, _, &low)| low + 1).sum::<u32>();
+    println!("part 1: {}", risk_level);
 
-            fn fits_in(inner: &str, outer: &str) -> bool {
-                inner.chars().all(|c| outer.contains(c))
+    let height = s.lines().count();
+    let width = s.lines().next().unwrap().len();
+
+    fn flood_fill(
+        g: &mut Vec<Vec<(usize, usize, u32)>>,
+        basin: (usize, usize),
+        (x, y): (usize, usize),
+    ) {
+        match g[y][x] {
+            (_, _, 9) => return,
+            (bx, by, _) if (bx, by) == basin => return,
+            _ => {
+                g[y][x] = (basin.0, basin.1, g[y][x].2);
+                if let Some(x) = x.checked_sub(1) {
+                    flood_fill(g, basin, (x, y));
+                }
+                if let Some(y) = y.checked_sub(1) {
+                    flood_fill(g, basin, (x, y));
+                }
+                if x + 1 < g[0].len() {
+                    flood_fill(g, basin, (x + 1, y));
+                }
+                if y + 1 < g.len() {
+                    flood_fill(g, basin, (x, y + 1));
+                }
             }
-            fn equals(a: &str, b: &str) -> bool {
-                fits_in(a, b) && fits_in(b, a)
+        }
+    }
+
+    fn not_separated_by_9(
+        grid: &Vec<Vec<u32>>,
+        (sx, sy): (usize, usize),
+        (ex, ey): (usize, usize),
+    ) -> bool {
+        let xmin = sx.min(ex);
+        let xmax = sx.max(ex);
+        let ymin = sy.min(ey);
+        let ymax = sy.max(ey);
+
+        let separated = (xmin..=xmax).all(|x| grid[ymin][x] < 9)
+            && (ymin..=ymax).all(|y| grid[y][xmax] < 9)
+            || (xmin..=xmax).all(|x| grid[ymax][x] < 9) && (ymin..=ymax).all(|y| grid[y][xmin] < 9);
+        println!(
+            "sx {} | sy {} | ex {} | ey {} | not_sep {}",
+            sx, sy, ex, ey, separated
+        );
+        separated
+    }
+
+    let mut grid = grid
+        .iter()
+        .enumerate()
+        .map(|(y, r)| r.iter().enumerate().map(|(x, &v)| (x, y, v)).collect_vec())
+        .collect_vec();
+
+    let basin = low_points
+        .iter()
+        .map(|&(x, y, _)| {
+            let basin = (x, y);
+            if let Some(x) = x.checked_sub(1) {
+                flood_fill(&mut grid, basin, (x, y));
             }
-            fn extract<'a>(collection: &'_ [&'a str], pred: impl FnMut(&&&str) -> bool) -> &'a str {
-                collection.into_iter().filter(pred).next().unwrap()
+            if let Some(y) = y.checked_sub(1) {
+                flood_fill(&mut grid, basin, (x, y));
+            }
+            if x + 1 < grid[0].len() {
+                flood_fill(&mut grid, basin, (x + 1, y));
+            }
+            if y + 1 < grid.len() {
+                flood_fill(&mut grid, basin, (x, y + 1));
             }
 
-            let one = extract(&input, |n| n.len() == 2);
-            let seven = extract(&input, |n| n.len() == 3);
-            let four = extract(&input, |n| n.len() == 4);
-            let eight = extract(&input, |n| n.len() == 7);
+            grid.iter()
+                .flat_map(|r| r.iter().filter(|(bx, by, _)| (*bx, *by) == basin))
+                .count() as u64
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .product::<u64>();
 
-            let one_of_235 = input.iter().filter(|n| n.len() == 5).copied().collect_vec();
-            let one_of_069 = input.iter().filter(|n| n.len() == 6).copied().collect_vec();
-
-            let (zero, six, nine) = one_of_069
-                .into_iter()
-                .sorted_by_key(|n| one_of_235.iter().filter(|i| fits_in(i, n)).count())
-                .collect_tuple()
-                .unwrap();
-
-            let five = extract(&one_of_235, |n| fits_in(n, six));
-            let three = extract(&one_of_235, |n| fits_in(one, n));
-
-            let mapping = vec![
-                (zero, 0),
-                (one, 1),
-                (three, 3),
-                (four, 4),
-                (five, 5),
-                (six, 6),
-                (seven, 7),
-                (eight, 8),
-                (nine, 9),
-            ];
-
-            let n = o
-                .split(' ')
-                .rev()
-                .enumerate()
-                .map(|(i, n)| {
-                    10_u64.pow(i as _)
-                        * mapping
-                            .iter()
-                            .find(|(c, _)| equals(n, c))
-                            .map_or(2, |(_, v)| *v)
-                })
-                .sum::<u64>();
-
-            s + n
-        });
-
-    println!("part 2: {}", sum);
+    println!("part 2: {}", basin);
 }
